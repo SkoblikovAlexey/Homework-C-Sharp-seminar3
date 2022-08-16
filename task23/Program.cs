58937985a33c50865aa155726314f5a9337aaa98
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-16 19:53:21 +0300

    Task 23 redact added

--- task23/Program.cs
+++ task23/Program.cs
@@ -6,8 +6,8 @@
 Console.WriteLine("Введите число.");
 int N = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 1; i <= N; i++) 
+for (int i = 1; i <= N; i++)
 {
-    Console.Write(Math.Pow(i, 3) + " ");   
+    Console.Write(Math.Pow(i, 3) + " ");
 }
 Console.WriteLine();
